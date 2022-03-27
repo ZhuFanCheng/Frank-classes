c0bb6b14b43a0088dc2b7fab66c8eec23d24669f
朱繁诚 <zhufancheng@zhufanchengdeMacBook-Pro.local>
2022-03-27 18:43:04 +0100

    added the if statement

--- Matlab-tutorial/ifs.m
+++ Matlab-tutorial/ifs.m
@@ -3,14 +3,14 @@
 % build an "if" statement to test if your classmate is older than you
 
 % how old are you?
-my_age = ...;
+my_age = 16;
     
 % how old is your classmate?
-...;
+classmate_age = 18;
     
 % your if statement...
-if ...
-        
-elseif ...
+if my_age>classmate_age:
+    display('I am older than my classmate')
+
         
 end
